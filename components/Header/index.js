b868b608d3d12d328f2df1bb92a6f6bfe021b253
carlsachs <sachscarl@gmail.com>
2020-01-17 11:07:42 -0500

    started adding elements and appending the header

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,17 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+const whole = document.querySelector('.header-container')
+
+function Header() {
+
+    const header = document.createElement('div')
+    const date = document.createElement('span')
+    const h1 = document.createElement('h1')
+    const temp = document.createElement('span')
+
+    whole.append(header)
+    header.append
+
+
+}
